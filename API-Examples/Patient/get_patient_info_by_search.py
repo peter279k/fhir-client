b009846836f8644b067552842db11a785f188822
peter279k <peter279k@gmail.com>
2021-10-07 10:40:55 +0800

    Update Patient info search with param set

--- API-Examples/Patient/get_patient_info_by_search.py
+++ API-Examples/Patient/get_patient_info_by_search.py
@@ -6,7 +6,7 @@ headers = {
     'Accept': 'application/fhir+json',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36',
 }
-search_parameters = '?identifier=S123456789'
+search_parameters = '?family=李&identifier=M123456789'
 fhir_server_url = 'https://fhir.dicom.tw/fhir/Patient' + search_parameters
 #fhir_server_url = 'https://hapi.fhir.tw/fhir/Patient' + search_parameters
 
